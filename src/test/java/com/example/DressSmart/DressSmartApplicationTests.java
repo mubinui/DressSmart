0e6be480cb470f104e80ac61c4300223009df27a
uicmu <mubin41@student.sust.edu>
2021-08-07 19:51:28 +0600

    Initial commit

---
+++ src/test/java/com/example/DressSmart/DressSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DressSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
